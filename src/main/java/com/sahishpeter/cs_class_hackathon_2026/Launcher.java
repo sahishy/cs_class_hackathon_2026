9fe6453b93420eb45e9827f27bb151f8ef6f0a1f
Sahish Durgam <91765380+sahishy@users.noreply.github.com>
2026-05-12 10:37:08 -0400

    root fit

---
+++ src/main/java/com/sahishpeter/cs_class_hackathon_2026/Launcher.java
@@ -0,0 +1,9 @@
+package com.sahishpeter.cs_class_hackathon_2026;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.sahishpeter.cs_class_hackathon_2026 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.sahishpeter.cs_class_hackathon_2026 to javafx.fxml;
+    exports com.sahishpeter.cs_class_hackathon_2026;
+}
